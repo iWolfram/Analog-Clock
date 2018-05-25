19f14740b2472154065fc8633f2b85a4bd491fd2
Rameses Resuena <rdresuena@live.com>
2018-05-24 23:47:27 -0600

    code to move clock hands

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+// for degrees
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
